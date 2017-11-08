54d6ca257459c1e58d86983ed7ee8366e156b1b4
jonjack <jonathan.x.jackson@gmail.com>
2017-11-08 10:51:39 +0000

    Housekeeping

--- src/main/scala/uk/co/britishgas/batch/oam/Client.scala
+++ src/main/scala/uk/co/britishgas/batch/oam/Client.scala
@@ -41,10 +41,10 @@ import scala.util.{Random, Try}
 trait Client {
 
   private val log: LoggingAdapter         = Logging.getLogger(system, this)
-  private val logrequest: LoggingAdapter  = Logging.getLogger(system, "requests")
-  private val logsuccess: LoggingAdapter  = Logging.getLogger(system, "success")
-  private val logfailure: LoggingAdapter  = Logging.getLogger(system, "failure")
-  private val loganalytics: LoggingAdapter  = Logging.getLogger(system, "analytics")
+  private val logRequest: LoggingAdapter  = Logging.getLogger(system, "requests")
+  private val logSuccess: LoggingAdapter  = Logging.getLogger(system, "success")
+  private val logFailure: LoggingAdapter  = Logging.getLogger(system, "failure")
+  private val logAnalytics: LoggingAdapter  = Logging.getLogger(system, "analytics")
 
   private val apiHost: String             = conf("api-host")
   private val apiPath: String             = conf("api-path")
@@ -60,7 +60,7 @@ trait Client {
 
   private val jobId: Int = Random.nextInt(100000000)
 
-  loganalytics.info(
+  logAnalytics.info(
     s"Starting BF job $jobId \n\n" +
       "Data source: " + file + "\n" +
       "Consuming endpoint: https://" + apiHost + ":" + apiPort + apiPath +
@@ -79,7 +79,8 @@ trait Client {
   /** A Flow which explicitly creates back pressure on it's upstream source of elements and
     * controls the rate at which elements are emitted downstream in the materialized graph.
     */
-  private val throttle = Flow[ByteString].throttle(throttleRate, 1.second, throttleBurst, ThrottleMode.shaping)
+  private val throttle: Flow[ByteString, ByteString, NotUsed] =
+    Flow[ByteString].throttle(throttleRate, 1.second, throttleBurst, ThrottleMode.shaping)
 
   /** A Flow of type ByteString => (String, String) which marshalls a stream of ByteStrings into a
     * JSON representation. The emitted tuple has a logical type of (Customer ID, Customer JSON).
@@ -104,7 +105,7 @@ trait Client {
   private val connection: Flow[(HttpRequest, String), (Try[HttpResponse], String), Http.HostConnectionPool] =
     Http().cachedHostConnectionPoolHttps[String](apiHost, apiPort)
 
-  private def extractId(jst: String) = jst.parseJson.asJsObject.fields("data").asJsObject.fields("id").toString
+  private def extractId(jst: String): String = jst.parseJson.asJsObject.fields("data").asJsObject.fields("id").toString
 
   import akka.http.scaladsl.model.HttpMethods._
   import akka.http.scaladsl.model.HttpProtocols._
@@ -128,7 +129,7 @@ trait Client {
       via(marshaller).
       map((tup: (String, String)) => (buildRequest(tup._2), tup._1)).
       map((tup: (HttpRequest, String)) => {
-        logrequest.info(tup._1.toString())
+        logRequest.info(tup._1.toString())
         (tup._1, tup._2)
       }).
       via(connection).
@@ -136,15 +137,15 @@ trait Client {
         case (map, (util.Success(resp), ucrn)) => {
           resp.status.intValue match {
             case 201 => resp.entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach { body =>
-              logsuccess.info("Created 201 [" + ucrn + "] BODY[" + body.utf8String + "]")
+              logSuccess.info("Created 201 [" + ucrn + "] BODY[" + body.utf8String + "]")
             }
             case status => resp.entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach { body =>
-              logfailure.info("Status " + status + " [" + ucrn + "] BODY[" + body.utf8String + "]")
+              logFailure.info("Status " + status + " [" + ucrn + "] BODY[" + body.utf8String + "]")
             }
           }
           map ++ Map(ucrn -> resp)}
         case (map, (util.Failure(ex), symbol)) => {
-          logfailure.info("Exception " + ex.getMessage)
+          logFailure.info("Exception " + ex.getMessage)
           map
         }
       }
@@ -153,9 +154,9 @@ trait Client {
   fut.onComplete((hr: Try[Map[String, HttpResponse]]) => {
     val respMap: Map[String, HttpResponse] = hr.get
     val total: Int = respMap.size
-    val successes: Int = respMap.filter(x => x._2.status.intValue() == 201).size
-    val failures: Int = respMap.filter(x => x._2.status.intValue() != 201).size
-    loganalytics.info {
+    val successes: Int = respMap.count(x => x._2.status.intValue() == 201)
+    val failures: Int = respMap.count(x => x._2.status.intValue() != 201)
+    logAnalytics.info {
       s"Terminating job $jobId \n\n" +
         s"/------------ Analytics (job $jobId) ------------/\n\n" +
         " Total elements processed  = " + respMap.size + "\n" +
